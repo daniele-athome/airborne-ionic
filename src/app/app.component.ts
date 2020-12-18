d29577cfcc345ea7dcc564515c19431adfe0e25e
Daniele Ricci <daniele@casaricci.it>
2020-12-18 13:24:27 +0100

    Assuming light mode for status bar (force in iOS)
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,26 @@ import { Title } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    private titleService: Title
-  ) {
-    this.initializeApp();
-  }
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar,
+        private titleService: Title
+    ) {
+        this.initializeApp();
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // won't use statusbar for now - this.statusBar.styleDefault();
-      this.statusBar.overlaysWebView(false);
-      // will be hidden later -- this.splashScreen.hide();
-      this.titleService.setTitle(this.titleService.getTitle() + ' (' + environment.aircraft.id + ')');
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            // assuming light mode
+            this.statusBar.styleDefault();
+            // will be hidden later -- this.splashScreen.hide();
+            this.titleService.setTitle(this.titleService.getTitle() + ' (' + environment.aircraft.id + ')');
+        });
+    }
 }
