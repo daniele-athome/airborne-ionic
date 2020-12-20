4a5bf8ef9de035c4ef0d735fe423e666f6dbb24d
Daniele Ricci <daniele@casaricci.it>
2020-12-20 22:51:31 +0100

    Somewhat working locale detection (#12)
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { APP_INITIALIZER, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { registerLocaleData } from '@angular/common';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Globalization } from '@ionic-native/globalization/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,9 +17,7 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
 import { HttpClientModule } from '@angular/common/http';
 import { ConfigService } from './services/config.service';
-
-import localeIt from '@angular/common/locales/it';
-registerLocaleData(localeIt);
+declare var System;
 
 // register FullCalendar plugins
 FullCalendarModule.registerPlugins([
@@ -27,6 +26,32 @@ FullCalendarModule.registerPlugins([
     timeGridPlugin,
     googleCalendarPlugin,
 ]);
+
+// FIXME refactor this mess
+let localeId;
+
+// FIXME refactor this mess
+function loadLocaleData() {
+    let languageCode;
+    if (localeId.indexOf('-') !== -1) {
+        languageCode = localeId.split('-')[0];
+    }
+    else if (localeId.indexOf('_') !== -1) {
+        languageCode = localeId.split('_')[0];
+    }
+    else {
+        languageCode = localeId;
+    }
+
+    console.log('loading locale data for ' + languageCode);
+    return new Promise((resolve, reject) => {
+        System.import(`@angular/common/locales/${languageCode}.js`)
+            .then(module => {
+                registerLocaleData(module.default);
+                resolve();
+            }, reject);
+    });
+}
 
 @NgModule({
     declarations: [AppComponent],
@@ -42,8 +67,44 @@ FullCalendarModule.registerPlugins([
         StatusBar,
         SplashScreen,
         ConfigService,
+        Globalization,
+        // FIXME refactor this mess
         {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
-        {provide: LOCALE_ID, useValue: 'it-IT'},
+        {
+            provide: APP_INITIALIZER,
+            multi: true,
+            deps: [Globalization, Platform],
+            useFactory: (globalization: Globalization, platform: Platform) => {
+                return (): Promise<void> => {
+                    return new Promise<void>((resolve) => {
+                        platform.ready().then(async (source) => {
+                            if (source === 'cordova') {
+                                const localeData = await globalization.getLocaleName();
+                                localeId = localeData.value;
+                            }
+                            else {
+                                localeId = 'it-IT';
+                            }
+                            loadLocaleData().then(() => resolve()).catch(() => {
+                                localeId = 'en-US';
+                                loadLocaleData().finally(() => {
+                                    resolve();
+                                });
+                            });
+                        });
+                    });
+                };
+            },
+        },
+        // FIXME refactor this mess
+        {
+            provide: LOCALE_ID,
+            deps: [APP_INITIALIZER],
+            useFactory: () => {
+                console.log('localeId=' + localeId);
+                return localeId;
+            }
+        },
     ],
     bootstrap: [AppComponent]
 })
