42c93535d896869b3941c10bef13ba2efde0d2a5
Daniele Ricci <daniele@casaricci.it>
2021-05-21 22:17:43 +0200

    Cleanup
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- src/app/pages/pilot-select/pilot-select.component.ts
+++ src/app/pages/pilot-select/pilot-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { ConfigService } from '../../services/config.service';
 import { AlertController, IonRouterOutlet, Platform, ViewDidEnter } from '@ionic/angular';
